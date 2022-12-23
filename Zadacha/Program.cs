c30b2f8150d4137434578e4681f010aa9087bd7d
maxbondarenko <bondarenkomm@bk.ru>
2022-12-23 14:19:29 +0500

    Create file Zadacha

---
+++ Zadacha/Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+string[] array = new string[4] {"54", "456", "hello", "ok"};
+string[] newarray = new string[array.Length];
+
+NewArray(array, newarray);
+PrintArray(newarray);
+
+void NewArray(string[] array, string[] newarray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        newarray[count] = array[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
